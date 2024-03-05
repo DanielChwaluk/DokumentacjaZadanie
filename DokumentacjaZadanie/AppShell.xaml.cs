6763cbd7110dc47de3b71723ee5782cdc940ada4
DanielCh <DanielCh@S213-14>
2024-03-05 12:41:52 +0100

    Dodaj pliki projektów.

---
+++ DokumentacjaZadanie/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DokumentacjaZadanie
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DokumentacjaZadanie/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace DokumentacjaZadanie
+{
+    public partial class MainPage : ContentPage
+    {
+        class Liczby
+        {
+            private int a1;
+            private int a2;
+            private int suma;
+            public Liczby(int b1, int b2)
+            {
+                a1 = b1;
+                a2 = b2;
+            }
+            public Liczby()
+            {
+                a1 = 0;
+                a2 = 0;
+            }
+            public int DodajLiczby(int b1, int b2)
+            {
+                suma = b1+ b2;
+                return suma;
+            }
+            public void PokazLiczby()
+            {
+                return $"Liczba 1:{a1} Liczba 2: ";
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
